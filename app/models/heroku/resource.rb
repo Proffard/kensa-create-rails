00801774a5706d64912b8980bee45e4c0b6c62a2
Proffard <prffrd@gmail.com>
2014-05-29 22:22:05 +0400

    add working heroku sso

--- app/models/heroku/resource.rb
+++ app/models/heroku/resource.rb
@@ -1,4 +1,4 @@
 class Heroku::Resource < ActiveRecord::Base
   self.table_name = 'heroku_resources'
-  devise :database_authenticatable, :trackable
+  devise  :database_authenticatable, :trackable , :heroku_sso
 end
